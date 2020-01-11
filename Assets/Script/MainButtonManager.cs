b53b54df5e4c6f360e9d7f71f3f9b0b86ffd82f8
au-niji <kouhei.hamu@gmail.com>
2020-01-11 13:28:17 +0900

    Disable zero button

---
+++ Assets/Script/MainButtonManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainButtonManager : MonoBehaviour
+{
+    //ゲーム時のボタンの有効化、無効化を管理します。
+
+    [SerializeField]
+    private Button Bt_Minus;
+    [SerializeField]
+    private Button Bt_Zero;
+    [SerializeField]
+    private Button Bt_Plus;
+    [SerializeField]
+    private Button Bt_Pause;
+    public void DisableMainButton()
+    {
+        //ボタンを押せなくします。
+        Bt_Minus.interactable = false;
+        Bt_Zero.interactable  = false;
+        Bt_Plus.interactable  = false;
+        Bt_Pause.interactable = false;
+    }
+    public void EnableMainButton()
+    {
+        //ボタンを押せるようにします。
+        Bt_Minus.interactable = true;
+        Bt_Zero.interactable  = true;
+        Bt_Plus.interactable  = true;
+        Bt_Pause.interactable = true;
+    }
+
+} 
